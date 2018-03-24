922478d46867c297686961f90050bcf52b7ef741
goldenwings <Worldwar3>
2018-03-25 01:27:20 +0200

    reflection

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from controller import config as controller
 from sensor import config as sensor
 from pilot import car as car
 from pilot import config as pilot
+import sys
+import inspect
 
 # Load Configuration
 
@@ -18,5 +20,8 @@ pilot_config = pilot.pilot_config
 """
 We can initialize objects from each active module based on configuration files
 """
-
+objects = [] # List of all objects
 # Initialize car Object
+for name, obj in inspect.getmembers(sys.modules[controller.__name__]):
+        if inspect.isclass(obj) and controller_config.modules['name'] == name:
+            objects.append(obj)
