838f434b131671776331bc49195ca0208f1ca162
MouhamedEssam <mouhamedessam@outlook.com>
2018-03-25 04:46:06 +0200

    initial commit of hardware config

---
+++ car/hardware/config.py
@@ -0,0 +1,16 @@
+"""
+This file will contain the configuration need for the hardware parts to work
+
+"""
+# Md10c
+MD10C_DIR_PIN = 7
+MD10C_PWM_PIN = 12
+MD10C_FREQ = 20000
+
+# Hcsr04
+PIN_DIST_TRIG = 40
+PIN_DIST_ECHO = 38
+PIN_DIST_ECHO2 = 37
+PIN_DIST_ECHO3 = 36
+PIN_DIST_ECHO4 = 35
+PIN_DIST_ECHO5 = 33
