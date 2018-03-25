ae732a278cd60c7d456d247037faece46f1a4514
MouhamedEssam <mouhamedessam@outlook.com>
2018-03-25 07:28:10 +0200

    initial commit for F710 class

--- car/hardware/f710.py
+++ car/hardware/f710.py
@@ -5,7 +5,6 @@ class F710:
     f710 = None
     __f710_name = ["Logitech Logitech Cordless RumblePad 2", "Logitech Gamepad F710"]
     __f710_path = ''
-    ___f710_founded = False
 
     @property
     def __find_f710(self):
@@ -25,3 +24,6 @@ class F710:
 
     def __init__(self):
         self.__set_f710()
+
+    def __del__(self):
+        self.f710 = None
