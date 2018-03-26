4172be837cc6aa466906030a96efc9e4e36efdf5
@GoldenWings <mohamedalzaki@outlook.com>
2018-03-26 19:43:00 +0200

    Make it even more independent
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

--- car/car.py
+++ car/car.py
@@ -1,34 +1,39 @@
 class Car:
     def __init__(self, objects, threaded_objects):
         self.__threaded_objects = threaded_objects
-        self.__motor = objects.get('Motor')
-        self.__servo = objects.get('Servo')
-        self.__status = objects.get('Status')
-        self.__camera = objects.get('Camera')
-        self.__ultrasonic = objects.get('Ultrasonic')
+        self.__objects = {**objects, **threaded_objects}
+
+    @property
+    def status(self):
+        return self.__objects['Status']
+
+    @property
+    def camera(self):
+        return self.__objects['Camera']
+
+    @property
+    def ultrasonic(self):
+        return self.__objects['Ultrasonic']
 
     def start_threads(self):
         for obj in self.__threaded_objects:
             obj.start()
 
     def turn_right(self):
-        self.__servo.turn_right()
+        self.__objects['Servo'].turn_right()
 
     def turn_left(self):
-        self.__servo.turn_left()
+        self.__objects['Servo'].turn_left()
 
     def move_forward(self):
-        self.__motor.move_forward()
+        self.__objects['Motor'].move_forward()
 
     def move_backward(self):
-        self.__motor.move_backward()
+        self.__objects['Motor'].move_backward()
 
     def brake(self):
-        self.__motor.brake()
+        self.__objects['Motor'].brake()
 
     def __del__(self):
-        del self.__motor
-        del self.__servo
-        del self.__camera
-        del self.__ultrasonic
-        del self.__status
+        del self.__objects
+        del self.__threaded_objects
