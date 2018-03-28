b7c570c05d0cebcc7f5c88153bb27660f0fead7d
MouhamedEssam <mouhamedessam@outlook.com>
2018-03-29 00:15:28 +0200

    added class_name and made name as module name

--- car/controller/config.py
+++ car/controller/config.py
@@ -13,14 +13,16 @@ motor_inputs = [
     {'input_name': 'movement_dir', 'input_type': 'integer'}
 ]
 motor_outputs = None
-motor_config = Config(name='Motor', inputs=motor_inputs, outputs=motor_outputs, is_active=True, is_threaded=False)
+motor_config = Config(name='motor', class_name='Motor', inputs=motor_inputs, outputs=motor_outputs, is_active=True,
+                      is_threaded=False)
 
 # Servo Configuration
 servo_inputs = [
     {'input_name': 'direction', 'input_type': 'integer'}
 ]
 servo_outputs = None
-servo_config = Config(name='Servo', inputs=servo_inputs, outputs=servo_outputs, is_active=True, is_threaded=False)
+servo_config = Config(name='servo', class_name='Servo', inputs=servo_inputs, outputs=servo_outputs, is_active=True,
+                      is_threaded=False)
 
 
 # Gamepad Configuration
@@ -28,7 +30,8 @@ gamepad_inputs = None
 gamepad_outputs = [
     {'output_name': 'movement_dir', 'output_type': 'integer'}, {'output_name': 'direction', 'output_type': 'integer'}
 ]
-gamepad_config = Config(name='Gamepad', inputs=gamepad_inputs, outputs=gamepad_outputs, is_active=True,
+gamepad_config = Config(name='gamepad', class_name='Gamepad', inputs=gamepad_inputs, outputs=gamepad_outputs,
+                        is_active=True,
                         is_threaded=True, parameters=['Car'])
 
 # Package Configuration
