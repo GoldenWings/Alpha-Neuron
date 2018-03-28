f1f29bbfb05fba38511c95b2dd98b6fe02de403c
MouhamedEssam <mouhamedessam@outlook.com>
2018-03-29 00:20:25 +0200

    added pi ip to allowed hosts

--- interface/settings.py
+++ interface/settings.py
@@ -125,4 +125,4 @@ STATICFILES_DIRS = (os.path.join('static'),)
 LOGIN_REDIRECT_URL = '/agent/'
 LOGIN_URL = '/account/login'
 
-ALLOWED_HOSTS = ['192.168.1.103', '41.43.142.246']
+ALLOWED_HOSTS = ['192.168.1.103', '41.43.142.246', '192.168.43.60']
