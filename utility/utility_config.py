93ee4ce49eac4c5979657c982b3d7e45469eb497
MouhamedEssam <mouhamedessam@outlook.com>
2018-03-29 00:22:49 +0200

    changed name to module name, added class_name

--- utility/utility_config.py
+++ utility/utility_config.py
@@ -1,6 +1,7 @@
 from utility.config import Config
 
-status_config = Config(name='Status', inputs=None, outputs=None, is_active=True, is_threaded=False)
+__name__ = 'utility.status'
+status_config = Config(name='status', class_name='Status', inputs=None, outputs=None, is_active=True, is_threaded=False)
 
 utility_modules = {status_config.name: status_config}
-utility_config = Config(name='Utility', modules=utility_modules)
+utility_config = Config(name='utility', modules=utility_modules)
