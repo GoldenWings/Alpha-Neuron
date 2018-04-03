1f4dfc920a0865bb9a997360484cb617253c3f52
@GoldenWings <mohamedalzaki@outlook.com>
2018-04-03 23:51:48 +0200

    create file if not exist
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

--- pilot/trainer/trainer_session.py
+++ pilot/trainer/trainer_session.py
@@ -5,5 +5,5 @@ from pilot.trainer.config import *
 def store_command(command):
     timestamp = datetime.now()
     entry = command + " " + str(timestamp)
-    with open(FILE_PATH, "a") as writer:
+    with open(FILE_PATH, "w+") as writer:
         writer.write(entry+'\n')
