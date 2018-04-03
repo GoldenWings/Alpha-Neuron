5a2af3b76758b2d654c141ddb490184fa970331c
@GoldenWings <mohamedalzaki@outlook.com>
2018-04-03 23:48:40 +0200

    initializing objects in separate method
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

--- car/car.py
+++ car/car.py
@@ -2,7 +2,11 @@ from utility.singleton import Singleton
 
 
 class Car(metaclass=Singleton):
-    def __init__(self, objects, threaded_objects):
+    def __init__(self):
+        self.__threaded_objects = None
+        self.__objects = None
+
+    def initialize_objects(self, objects, threaded_objects):
         self.__threaded_objects = threaded_objects
         self.__objects = {**objects, **threaded_objects}
         self.start_threads()
@@ -20,32 +24,34 @@ class Car(metaclass=Singleton):
         return self.__objects['UltrasonicFrame']
 
     def start_threads(self):
-        for obj in self.__threaded_objects:
+        for obj in self.__threaded_objects.values():
+            if obj is self:
+                continue
             obj.start()
 
     def turn_right(self):
-        self.__objects['Servo'].turn_right()
+        self.__objects['servo'].turn_right()
 
     def turn_left(self):
-        self.__objects['Servo'].turn_left()
+        self.__objects['servo'].turn_left()
 
     def move_forward(self):
-        self.__objects['Motor'].move_forward()
+        self.__objects['motor'].move_forward()
 
     def move_backward(self):
-        self.__objects['Motor'].move_backward()
+        self.__objects['motor'].move_backward()
 
     def inc_speed(self):
-        self.__objects['Motor'].inc_speed()
+        self.__objects['motor'].inc_speed()
 
     def dec_speed(self):
-        self.__objects['Motor'].dec_speed()
+        self.__objects['motor'].dec_speed()
 
     def current_speed(self):
-        self.__objects['Motor'].get_speed()
+        self.__objects['motor'].get_speed()
 
     def brake(self):
-        self.__objects['Motor'].brake()
+        self.__objects['motor'].brake()
 
     def __del__(self):
         del self.__objects
