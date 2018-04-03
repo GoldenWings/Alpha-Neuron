437966ccabd47cb5839364a4280faa02c1ade030
@GoldenWings <mohamedalzaki@outlook.com>
2018-04-03 23:52:47 +0200

    changed names of configuration
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

--- utility/utility_config.py
+++ utility/utility_config.py
@@ -1,6 +1,4 @@
 from utility.config import Config
-
-__name__ = 'utility.status'
 status_config = Config(name='status', class_name='Status', inputs=None, outputs=None, is_active=True, is_threaded=False)
 
 utility_modules = {status_config.name: status_config}
