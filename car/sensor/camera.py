3a6b08b89b1cc5663f578adf559c7febcc84b3ff
@GoldenWings <mohamedalzaki@outlook.com>
2018-04-09 02:08:14 +0200

    Use IOBytes instead of PIRGPARRAY for streaming compatibility
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

--- car/sensor/camera.py
+++ car/sensor/camera.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from threading import Thread
 import cv2
 import picamera
-from picamera.array import PiRGBArray
+from picamera.array import PiRGBArray, bytes_to_rgb
+import io
+import numpy as np
+from PIL import Image
 
 
 class PiCamera(Thread):
-    def __init__(self, objects, resolution=(640, 480), framerate=32):
+    def __init__(self, objects, resolution=(160, 120), framerate=20):
         Thread.__init__(self)
         # initialize the camera and stream
         self.status = objects.get('status')
@@ -16,11 +19,13 @@ class PiCamera(Thread):
         self.framerate = framerate
         self.camera = None
         self.rawCapture = None
+        self.continuous_stream = None
         self.stream = None
         self.initialize_sensor()
         # initialize the frame
         self.frame = None
         self.img_name = ''
+        self.byte_frame = None
 
     def initialize_sensor(self):
         if self.camera is not None:
@@ -29,8 +34,9 @@ class PiCamera(Thread):
         self.camera.resolution = self.resolution
         self.camera.framerate = self.framerate
         self.rawCapture = PiRGBArray(self.camera, size=self.resolution)
-        self.stream = self.camera.capture_continuous(self.rawCapture,
-                                                     format="bgr", use_video_port=True)
+        self.stream = io.BytesIO()
+        self.continuous_stream = self.camera.capture_continuous(self.stream,
+                                                     format="jpeg", use_video_port=True)
 
     def read(self):
             # return the frame most recently read
@@ -38,13 +44,16 @@ class PiCamera(Thread):
 
     def run(self):
         # keep looping infinitely until the thread is stopped
-        for f in self.stream:
+        i = 0
+        for f in self.continuous_stream:
             # grab the frame from the stream and clear the stream in
             # preparation for the next frame
-            self.frame = f.array
+            self.byte_frame = self.stream.getvalue()
+            self.frame = np.array(Image.open(io.BytesIO(self.byte_frame)))
+            #            self.frame = bytes_to_rgb(self.stream.getvalue(), self.camera.resolution)
             if self.status.is_recording:
                 i = 1
-                dirpath = str(Path(os.path.abspath(os.path.dirname(__file__))).parent)
+                dirpath = str(Path(os.path.abspath(os.path.dirname(__file__))).parent.parent)
                 filepath = dirpath + '/Training Data/Images/'
                 while os.path.exists(filepath + 'image_' + "%s.jpg" % i):
                     i += 1
@@ -58,7 +67,9 @@ class PiCamera(Thread):
             elif self.status.is_agent and self.agent is not None:
                 self.agent.put(self.frame)
 
-            self.rawCapture.truncate(0)
+            self.stream.seek(0)
+            self.stream.truncate()
+#            self.rawCapture.truncate(0)
 
     def __close(self):
         self.stream.close()
