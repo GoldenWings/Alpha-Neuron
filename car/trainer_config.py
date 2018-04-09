fdf874dd46bc95695f08cd8951b00ac65379a810
@GoldenWings <mohamedalzaki@outlook.com>
2018-04-09 23:03:15 +0200

    Training configuration and helper function
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

---
+++ car/trainer_config.py
@@ -0,0 +1,13 @@
+import os
+BATCH_SIZE = 128
+TRAIN_TEST_SPLIT = 0.8
+MODEL_PATH = '/pilot/models/'
+
+
+def count_models():
+    count = 0
+    for name in os.listdir(MODEL_PATH):
+        if os.path.isfile(os.path.join(MODEL_PATH, name)):
+            if os.path.splitext(name)[1] == '.hd5':
+                count += 1
+    return count
