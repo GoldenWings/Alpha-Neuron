8a88980a8bda598a0e96dc8488bbc392ee104926
@GoldenWings <mohamedalzaki@outlook.com>
2018-04-10 00:31:03 +0200

    Remove un-needed trainer class
    Signed-off-by: @GoldenWings <mohamedalzaki@outlook.com>

--- pilot/trainer/trainer_session.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from pilot.trainer.config import *
-
-
-def store_command(command):
-    timestamp = datetime.now()
-    entry = command + " " + str(timestamp)
-    with open(FILE_PATH, "w+") as writer:
-        writer.write(entry+'\n')
