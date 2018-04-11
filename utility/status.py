9938062fc1c5cca84813b2ab70fdcb43421475f0
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-11 12:34:27 +0200

    changing double Underscore to single depending on the usage

--- utility/status.py
+++ utility/status.py
@@ -4,26 +4,26 @@ from utility.singleton import Singleton
 class Status(metaclass=Singleton):
     def __init__(self):
         super().__init__()
-        self.__is_recording = False
-        self.__is_agent = False
-        self.__is_trainer = True
-        self.__is_paused = False
+        self._is_recording = False
+        self._is_agent = False
+        self._is_trainer = True
+        self._is_paused = False
 
     @property
     def is_trainer(self):
-        return self.__is_trainer
+        return self._is_trainer
 
     @property
     def is_agent(self):
-        return self.__is_agent
+        return self._is_agent
     
     @property
     def is_recording(self):
-        return self.__is_recording
+        return self._is_recording
 
     @property
     def is_paused(self):
-        return self.__is_paused
+        return self._is_paused
 
     @property
     def sensor_started(self):
@@ -31,33 +31,33 @@ class Status(metaclass=Singleton):
 
     def activate_agent(self):
         self.deactivate_trainer()
-        self.__is_agent = True
+        self._is_agent = True
 
     def activate_trainer(self):
         self.deactivate_agent()
-        self.__is_trainer = True
+        self._is_trainer = True
 
     def deactivate_agent(self):
-        self.__is_agent = False
+        self._is_agent = False
 
     def deactivate_trainer(self):
-        self.__is_trainer = False
+        self._is_trainer = False
         self.stop_recording()
 
     def start_recording(self):
-        self.__is_recording = True
+        self._is_recording = True
 
     def pause_recording(self):
-        self.__is_recording = False
-        self.__is_paused = True
+        self._is_recording = False
+        self._is_paused = True
 
     def continue_recording(self):
-        self.__is_paused = False
-        self.__is_recording  = True
+        self._is_paused = False
+        self._is_recording = True
 
     def stop_recording(self):
-        self.__is_recording = False
+        self._is_recording = False
 
     def reset_recording_status(self):
-        self.__is_recording = False
-        self.__is_paused = False
+        self._is_recording = False
+        self._is_paused = False
