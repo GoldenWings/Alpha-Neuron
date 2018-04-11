bc15f88912433ebd9cea3b6bc125d561280b0196
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-11 13:55:26 +0200

    updating __init__ file in car package

--- car/__init__.py
+++ car/__init__.py
@@ -3,5 +3,5 @@ __all__ = ['car', 'trainer_config', 'controller', 'hardware', 'sensor']
 from car import controller
 from car import hardware
 from car import sensor
-from car import trainer_config
 from car.car import Car
+from . import trainer_config
