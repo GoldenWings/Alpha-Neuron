e847f00ad07a78964f40f1d1996b4bf91ad5d58c
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-11 11:48:52 +0200

    changing double Underscore to single depending on the usage

--- car/car.py
+++ car/car.py
@@ -6,74 +6,74 @@ from utility.singleton import Singleton
 
 class Car(metaclass=Singleton):
     def __init__(self):
-        self.__threaded_objects = None
-        self.__objects = None
-        self.__sensor_objects = None
-        self.__is_started = False
+        self._threaded_objects = None
+        self._objects = None
+        self._sensor_objects = None
+        self._is_started = False
 
     def initialize_objects(self, objects, threaded_objects, sensor_objects):
-        self.__threaded_objects = threaded_objects
-        self.__objects = {**objects, **threaded_objects}
-        self.__sensor_objects = sensor_objects
-        self.__objects = {**self.__objects, **self.__sensor_objects}
+        self._threaded_objects = threaded_objects
+        self._objects = {**objects, **threaded_objects}
+        self._sensor_objects = sensor_objects
+        self._objects = {**self._objects, **self._sensor_objects}
         self.start_car()
 
     @property
     def status(self):
-        return self.__objects['status']
+        return self._objects['status']
 
     @property
     def camera(self):
-        return self.__objects['camera']
+        return self._objects['camera']
 
     @property
     def ultrasonic(self):
-        return self.__objects['ultrasonic']
+        return self._objects['ultrasonic']
 
     def start_sensor(self):
-        for o in self.__sensor_objects.values():
+        for o in self._sensor_objects.values():
             o.start()
 
     def start_threads(self):
-        for obj in self.__threaded_objects.values():
+        for obj in self._threaded_objects.values():
             if obj is self:
                 continue
             obj.start()
 
     def turn_right(self):
-        self.__objects['servo'].turn_right()
+        self._objects['servo'].turn_right()
 
     def turn_left(self):
-        self.__objects['servo'].turn_left()
+        self._objects['servo'].turn_left()
 
     @property
     def current_angle(self):
-        return self.__objects['servo'].angle
+        return self._objects['servo'].angle
 
     def move_forward(self):
-        self.__objects['motor'].move_forward()
+        self._objects['motor'].move_forward()
 
     def move_backward(self):
-        self.__objects['motor'].move_backward()
+        self._objects['motor'].move_backward()
 
     def inc_speed(self):
-        self.__objects['motor'].inc_speed()
+        self._objects['motor'].inc_speed()
 
     def dec_speed(self):
-        self.__objects['motor'].dec_speed()
+        self._objects['motor'].dec_speed()
 
     @property
     def current_speed(self):
-        return self.__objects['motor'].throttle
+        return self._objects['motor'].throttle
 
     def brake(self):
-        self.__objects['motor'].brake()
+        self._objects['motor'].brake()
 
     def start_car(self, status_is_agent=False):
-        if not self.__is_started:
+        if not self._is_started:
             self.start_sensor()
             self.start_threads()
-            self.__is_started = True
+            self._is_started = True
         if status_is_agent:
             self.start_threads()
 
@@ -97,13 +97,13 @@ class Car(metaclass=Singleton):
 
     @property
     def barrel_reader(self):
-        return self.__objects['barrelreader']
+        return self._objects['barrelreader']
 
     @property
     def driving_nn(self):
-        return self.__objects['driving_nn']
+        return self._objects['driving_nn']
 
     def __del__(self):
-        del self.__sensor_objects
-        del self.__objects
-        del self.__threaded_objects
+        del self._sensor_objects
+        del self._objects
+        del self._threaded_objects
