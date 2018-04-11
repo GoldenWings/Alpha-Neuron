22452a2a3a4f8a359fdd43272ba7c86ff39b47be
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-11 12:50:05 +0200

    commenting the class methods

--- car/controller/motor.py
+++ car/controller/motor.py
@@ -10,6 +10,10 @@ class Motor(Md10c, metaclass=Singleton):
         self._current_speed = BRAKES
 
     def move_backward(self):
+        """
+        This method give the car motor an order to move backward.
+        :return: it has no return type.
+        """
         if self.get_dir():
             self.brake()
             self.set_dir(False)
@@ -20,6 +24,10 @@ class Motor(Md10c, metaclass=Singleton):
         self.set_speed(self._current_speed)
 
     def move_forward(self):
+        """
+        This method give the car motor an order to move forward.
+        :return: it has no return type.
+        """
         if not self.get_dir():
             self.brake()
             self.set_dir(True)
@@ -30,24 +38,38 @@ class Motor(Md10c, metaclass=Singleton):
         self.set_speed(self._current_speed)
 
     def brake(self):
+        """
+        This method used to set the car motor speed to zero (breaks)
+        :return: it has no return value
+        """
         self.set_speed(BRAKES)
 
     def inc_speed(self):
+        """
+        This function is used to increase the car motor speed.
+        :return: it has no return type
+        """
         if (self._current_speed + MOTOR_STEP) > MOTOR_MAX_SPEED:
-            print("The maximum speed reached")
             return
         self._current_speed += MOTOR_STEP
         self.set_speed(self._current_speed)
 
     def dec_speed(self):
+        """
+        This method is used to decrease the car motor speed.
+        :return: it has no return type
+        """
         if (self._current_speed - MOTOR_STEP) < MOTOR_MIN_SPEED:
-            print("The minimum speed reached")
             return
         self._current_speed -= MOTOR_STEP
         self.set_speed(self._current_speed)
 
     @property
     def throttle(self):
+        """
+        This function return the motor current speed.
+        :return: current speed
+        """
         return self._current_speed
 
     def __del__(self):
