2daff7e3cdb88a77fa3225aff7182a5ea7daad81
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-11 12:58:35 +0200

    commenting the class methods

--- car/hardware/f710.py
+++ car/hardware/f710.py
@@ -8,7 +8,10 @@ class F710:
 
     @property
     def _find_f710(self):
-
+        """
+        This method search for a Logitech f710 gamepad and if founded it gets it path and set it to _f710_path
+        :return: return a boolean ex:True if gamepad founded
+        """
         devices = [InputDevice(fn) for fn in list_devices()]
         for device in devices:
             if device.name in self._f710_name:
@@ -16,7 +19,10 @@ class F710:
                 return True
 
     def _set_f710(self):
-        # Open the device
+        """
+        This method initialize the gamepad if founded
+        :return: it has no return type
+        """
         if self._find_f710:
             self.f710 = InputDevice(self._f710_path)
         else:
