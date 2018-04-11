d2d072fe8ec9ec8a4fd36a3d37ef058754903be4
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-11 11:43:20 +0200

    changing double Underscore to single depending on the usage

--- car/controller/motor.py
+++ car/controller/motor.py
@@ -7,48 +7,48 @@ class Motor(Md10c, metaclass=Singleton):
 
     def __init__(self):
         super().__init__()
-        self.__current_speed = BRAKES
+        self._current_speed = BRAKES
 
     def move_backward(self):
         if self.get_dir():
             self.brake()
             self.set_dir(False)
-            self.__current_speed = MOTOR_MIN_SPEED
-            self.set_speed(self.__current_speed)
+            self._current_speed = MOTOR_MIN_SPEED
+            self.set_speed(self._current_speed)
         else:
             return
-        self.set_speed(self.__current_speed)
+        self.set_speed(self._current_speed)
 
     def move_forward(self):
         if not self.get_dir():
             self.brake()
             self.set_dir(True)
-            self.__current_speed = MOTOR_MIN_SPEED
-            self.set_speed(self.__current_speed)
+            self._current_speed = MOTOR_MIN_SPEED
+            self.set_speed(self._current_speed)
         else:
             return
-        self.set_speed(self.__current_speed)
+        self.set_speed(self._current_speed)
 
     def brake(self):
         self.set_speed(BRAKES)
 
     def inc_speed(self):
-        if (self.__current_speed + MOTOR_STEP) > MOTOR_MAX_SPEED:
+        if (self._current_speed + MOTOR_STEP) > MOTOR_MAX_SPEED:
             print("The maximum speed reached")
             return
-        self.__current_speed += MOTOR_STEP
-        self.set_speed(self.__current_speed)
+        self._current_speed += MOTOR_STEP
+        self.set_speed(self._current_speed)
 
     def dec_speed(self):
-        if (self.__current_speed - MOTOR_STEP) < MOTOR_MIN_SPEED:
+        if (self._current_speed - MOTOR_STEP) < MOTOR_MIN_SPEED:
             print("The minimum speed reached")
             return
-        self.__current_speed -= MOTOR_STEP
-        self.set_speed(self.__current_speed)
+        self._current_speed -= MOTOR_STEP
+        self.set_speed(self._current_speed)
 
     @property
     def throttle(self):
-        return self.__current_speed
+        return self._current_speed
 
     def __del__(self):
         super().__del__()
