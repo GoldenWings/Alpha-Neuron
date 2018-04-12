cc12ac85b599b76a11dbd54fc771d19349618015
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-12 16:09:13 +0200

    updating set_angle

--- car/hardware/pca9685.py
+++ car/hardware/pca9685.py
@@ -7,14 +7,14 @@ from car.hardware.config import (SERVO_MIN_PULSE, SERVO_MAX_PULSE, SERVO_MAX_ANG
 class Pca9685:
     def __init__(self, starting_angle):
         self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwm.set_pwm_freq(60)
         self.pulse_min = SERVO_MIN_PULSE
         self.pulse_max = SERVO_MAX_PULSE
         self.angle_max = SERVO_MAX_ANGLE
         self.angle_min = SERVO_MIN_ANGLE
         self.set_angle(starting_angle)
-        self.pwm.set_pwm_freq(60)
 
-    def set_angle(self, req_angle, channel=SERVO_CHANEL):
+    def set_angle(self, req_angle):
         """
         This method take the channel that servo is connected to in pca9685 board to send to it the
         pulse the represent the required angle using the equation
@@ -25,4 +25,4 @@ class Pca9685:
         """
         req_angle_pulse = ((self.angle_max - self.angle_min) / (self.pulse_max - self.pulse_min)) * (
                 req_angle - self.pulse_max) + self.angle_max
-        self.pwm.set_pwm(channel, 0, int(req_angle_pulse))
+        self.pwm.set_pwm(SERVO_CHANEL, 0, int(req_angle_pulse))
