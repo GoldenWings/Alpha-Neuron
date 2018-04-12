c9261be9b135a7e70d0f1df454275129aacd8af8
MouhamedEssam <mouhamedessam@outlook.com>
2018-04-12 16:06:57 +0200

    updating servo class to fit with the new structure of new set_angle

--- car/controller/servo.py
+++ car/controller/servo.py
@@ -1,20 +1,25 @@
-from car.hardware.config import SERVO_STEP
+from car.hardware.config import (SERVO_STEP, SERVO_EFFECTIVE_ANGLE, SERVO_ZERO_POS)
 from car.hardware.pca9685 import Pca9685
 from utility.singleton import Singleton
 
 
 class Servo(Pca9685, metaclass=Singleton):
     def __init__(self):
-        super().__init__()
-        self.step = SERVO_STEP
+        self._current_angle = SERVO_ZERO_POS
+        super().__init__(self._current_angle)
+        self._step = SERVO_STEP
 
     def turn_right(self):
         """This method turn the car servo angel to the right by value of step."""
-        self.set_angle((self.current_angle + self.step))
+        if (self._current_angle + self._step) in range(SERVO_EFFECTIVE_ANGLE[0], SERVO_EFFECTIVE_ANGLE[1]):
+            self.set_angle((self._current_angle + self._step))
+            self._current_angle += self._step
 
     def turn_left(self):
         """This method turn the car servo angel to the left by value of step."""
-        self.set_angle((self.current_angle - self.step))
+        if (self._current_angle - self._step) in range(SERVO_EFFECTIVE_ANGLE[0], SERVO_EFFECTIVE_ANGLE[1]):
+            self.set_angle((self._current_angle - self._step))
+            self._current_angle -= self._step
 
     @property
     def angle(self):
